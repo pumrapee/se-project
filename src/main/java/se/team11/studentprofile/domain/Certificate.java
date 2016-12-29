8762fb83a12cfd40d66415a6e45c927e5d642d01
b5702305 <tiazarias@gmail.com>
2016-12-29 22:42:19 +0700

    Edit entity Certificate - fixes #478

--- src/main/java/se/team11/studentprofile/domain/Certificate.java
+++ src/main/java/se/team11/studentprofile/domain/Certificate.java
@@ -2,9 +2,10 @@ package se.team11.studentprofile.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.Column;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
-
+import javax.validation.constraints.Size;
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
@@ -12,12 +13,15 @@ public class Certificate {
 
     /**
      */
+    @NotNull
     @ManyToOne
     private Student Student;
 
     /**
      */
     @NotNull
+    @Column(unique = true)    
+    @Size(min = 8, max = 8)
     private String CertificateID;
 
     /**
@@ -27,6 +31,7 @@ public class Certificate {
 
     /**
      */
+    @NotNull
     @ManyToOne
     private HistoryInActivity HistoryInActivity;
 }
