fef6567486d3d0deb9a3cf5aec1c8ba591831a9e
b5702305 <tiazarias@gmail.com>
2017-01-17 12:10:00 +0700

    add Entity RegisterOfActivity - fixes #794

---
+++ src/main/java/se/team11/studentprofile/domain/RegisterOfActivity.java
@@ -0,0 +1,34 @@
+package se.team11.studentprofile.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class RegisterOfActivity {
+
+    /**
+     */
+    @NotNull
+    @ManyToOne
+    private Student Student;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 11, max = 11)
+    private String RegisterOfActivityID;
+
+    /**
+     */
+    @ManyToOne
+    private AnnouncementOfActivity AnnouncementOfActivity;
+
+    /**
+     */
+    private Boolean Status;
+}
